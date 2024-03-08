301369611f6ed05df78eafb9621625596ed2fcd2
OmShirke <113794365+OmShirke@users.noreply.github.com>
2024-03-09 00:48:00 +0530

    ...

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,1 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const Register = () => {
+  const navigate = useNavigate();
+};
